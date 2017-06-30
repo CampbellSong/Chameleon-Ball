d62da7be43c8d215d70c1471b444e5397066106d
CampbellSong <763396457@qq.com>
2017-06-30 12:44:14 +0800

    Knif create
    seperate ball

--- Assets/Ball/BallScript.cs
+++ Assets/Ball/BallScript.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour {
-    public GameObject ball;
+   // public GameObject ball;
+
+    GameObject _ball;
 	// Use this for initialization
 	void Start () {
        // Destroy(ball,1);
+      // _ball = ball 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
 	}
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == GameObject.FindGameObjectWithTag("knif").tag) {
 
-        }
-    } 
+    
 }
