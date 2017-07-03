a75861f6f85db33f5bdb0ed67a816a4d3d322b63
CanTinGit <ctxx61@gmail.com>
2017-07-03 10:03:07 +0800

    fix mouse error with build 1.3version

--- Assets/Script/Change.cs
+++ Assets/Script/Change.cs
@@ -11,20 +11,10 @@ public class Change : MonoBehaviour {
     public Texture2D normalCursor;
     //这个变量 设置 光标是活跃的(true Or false)  
     public bool showCursor = false;
-    //[DllImport("User32.DLL")]
-    //public static extern IntPtr LoadCursorFromFile(string fileName);
 
-    //[DllImport("User32.DLL")]
-    //public static extern bool SetSystemCursor(IntPtr hcur, uint id);
-    //public const uint OCR_NORMAL = 32512;
-    //[DllImport("User32.DLL")]
-    //public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
-    //public const uint SPI_SETCURSORS = 87;
-    //public const uint SPIF_SENDWININICHANGE = 2;
-    //public string path;
     void Start()
     {
-        Cursor.SetCursor(normalCursor, Vector2.one, CursorMode.Auto);
+        //Cursor.SetCursor(normalCursor, Vector2.one, CursorMode.Auto);
     }
     void OnMouseOver()
     {
@@ -37,35 +27,8 @@ public class Change : MonoBehaviour {
     void OnMouseExit()
     {
         //重置默认光标  
-        Cursor.SetCursor(normalCursor, Vector2.one, CursorMode.Auto);
+        Cursor.SetCursor(null, Vector2.one, CursorMode.Auto);
         //showCursor变量设置为false  
         this.showCursor = false;
     }
-    //void Start()
-    //{
-    //    path = UnityEngine.Application.dataPath;
-    //    int num = path.LastIndexOf("/");
-    //    path = path.Substring(0, num);
-    //}
-    //void FixedUpdate()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        //鼠标点击图标  
-    //        IntPtr hcur_click = LoadCursorFromFile(path + "/click.cur");
-    //        SetSystemCursor(hcur_click, OCR_NORMAL);
-    //    }
-
-    //    if (Input.GetMouseButtonUp(0))
-    //    {
-    //        //正常状态图标  
-    //        IntPtr hcur_normal = LoadCursorFromFile(path + "/normal.cur");
-    //        SetSystemCursor(hcur_normal, OCR_NORMAL);
-    //    }
-    //}
-    //void OnApplicationQuit()
-    //{
-    //    //恢复为系统默认图标  
-    //    SystemParametersInfo(SPI_SETCURSORS, 0, IntPtr.Zero, SPIF_SENDWININICHANGE);
-    //}
 }
