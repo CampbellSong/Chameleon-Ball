3b0d6625fe97eecd9b755b456ada29a2cea27bcd
CanTinGit <ctxx61@gmail.com>
2017-07-03 11:23:14 +0800

    add gameOver script and fix some mistakes

---
+++ Assets/gameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameOver : MonoBehaviour
+{
+
+    public GameObject playAgain;
+    // Use this for initialization
+    private void OnTriggerStay(Collider other)
+    {
+        //  Debug.Log("111");
+        playAgain.SetActive(true);
+        Destroy(other.gameObject);
+    }
+}
